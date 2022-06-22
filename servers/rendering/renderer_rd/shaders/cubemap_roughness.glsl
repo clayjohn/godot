4a0d9f1d3d28d78e2dbbee3af332096b515a47c9
clayjohn <claynjohn@gmail.com>
2022-06-21 19:12:08 -0700

    WIP

--- servers/rendering/renderer_rd/shaders/cubemap_roughness.glsl
+++ servers/rendering/renderer_rd/shaders/cubemap_roughness.glsl
@@ -4,7 +4,7 @@
 
 #VERSION_DEFINES
 
-#define GROUP_SIZE 8
+#define GROUP_SIZE 4
 
 layout(local_size_x = GROUP_SIZE, local_size_y = GROUP_SIZE, local_size_z = 1) in;
 
@@ -13,6 +13,10 @@ layout(set = 0, binding = 0) uniform samplerCube source_cube;
 layout(rgba16f, set = 1, binding = 0) uniform restrict writeonly imageCube dest_cubemap;
 
 #include "cubemap_roughness_inc.glsl"
+
+#define SAMPLE_COUNT 32
+
+shared vec4 sample_directions[SAMPLE_COUNT];
 
 void main() {
 	uvec3 id = gl_GlobalInvocationID;
@@ -24,8 +28,6 @@ void main() {
 	if (params.use_direct_write) {
 		imageStore(dest_cubemap, ivec3(id), vec4(texture(source_cube, N).rgb, 1.0));
 	} else {
-		vec4 sum = vec4(0.0, 0.0, 0.0, 0.0);
-
 		float solid_angle_texel = 4.0 * M_PI / (6.0 * params.face_size * params.face_size);
 		float roughness2 = params.roughness * params.roughness;
 		float roughness4 = roughness2 * roughness2;
@@ -35,25 +37,37 @@ void main() {
 		T[1] = cross(N, T[0]);
 		T[2] = N;
 
-		for (uint sampleNum = 0u; sampleNum < params.sample_count; sampleNum++) {
-			vec2 xi = Hammersley(sampleNum, params.sample_count);
+		//Precompute sample directions instead of every pixel doing it
+		// 4x4 block so each pixel is responsible for 2
+		uint index = gl_LocalInvocationID.x * 2 + gl_LocalInvocationID.y * 8;
 
-			vec3 H = T * ImportanceSampleGGX(xi, roughness4);
-			float NdotH = dot(N, H);
-			vec3 L = (2.0 * NdotH * H - N);
+		vec2 xi = Hammersley(index, SAMPLE_COUNT);
+		vec3 H = ImportanceSampleGGX(xi, roughness4);
+		sample_directions[index].xyz = (2.0 * H.z * H - vec3(0.0, 0.0, 1.0));
+		float D = DistributionGGX(H.z, roughness4);
+		float pdf = D * H.z / (4.0 * H.z) + 0.0001;
+		float solid_angle_sample = 1.0 / (float(SAMPLE_COUNT) * pdf + 0.0001);
+		sample_directions[index].w = params.roughness == 0.0 ? 0.0 : 0.5 * log2(solid_angle_sample / solid_angle_texel);
 
-			float ndotl = clamp(dot(N, L), 0.0, 1.0);
+		xi = Hammersley(index + 1, SAMPLE_COUNT);
+		H = ImportanceSampleGGX(xi, roughness4);
 
-			if (ndotl > 0.0) {
-				float D = DistributionGGX(NdotH, roughness4);
-				float pdf = D * NdotH / (4.0 * NdotH) + 0.0001;
+		sample_directions[index + 1].xyz = (2.0 * H.z * H - vec3(0.0, 0.0, 1.0));
+		D = DistributionGGX(H.z, roughness4);
+		pdf = D * H.z / (4.0 * H.z) + 0.0001;
+		solid_angle_sample = 1.0 / (float(SAMPLE_COUNT) * pdf + 0.0001);
+		sample_directions[index].w = params.roughness == 0.0 ? 0.0 : 0.5 * log2(solid_angle_sample / solid_angle_texel);
 
-				float solid_angle_sample = 1.0 / (float(params.sample_count) * pdf + 0.0001);
+		memoryBarrierShared();
+		barrier();
 
-				float mipLevel = params.roughness == 0.0 ? 0.0 : 0.5 * log2(solid_angle_sample / solid_angle_texel);
+		vec4 sum = vec4(0.0, 0.0, 0.0, 0.0);
 
-				sum.rgb += textureLod(source_cube, L, mipLevel).rgb * ndotl;
-				sum.a += ndotl;
+		for (uint sampleNum = 0u; sampleNum < SAMPLE_COUNT; sampleNum++) {
+			if (sample_directions[sampleNum].z > 0.0) {
+				vec3 L = T * sample_directions[sampleNum].xyz;
+				sum.rgb += textureLod(source_cube, L, sample_directions[sampleNum].w).rgb * sample_directions[sampleNum].z;
+				sum.a += sample_directions[sampleNum].z;
 			}
 		}
 		sum /= sum.a;
